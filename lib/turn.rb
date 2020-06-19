329af31e62e87bd14fd3a948c00c4c31b48aad3e
Chandler Hanson <chan.hanson5@gmail.com>
2020-06-19 23:04:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,27 @@ end
 def turn(board)
   puts "Please enter 1-9:"
 end
+def valid_move?(board, index)
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def on_board?(index)
+    if index.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+end
